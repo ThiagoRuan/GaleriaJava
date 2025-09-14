16f18e7df44bcac1c5876558d0cc18a0b7e355cc
ThiagoRuan <thiagoruan46@gmail.com>
2025-09-14 09:55:38 -0300

    initial commit

---
+++ src/main/java/com/galeria/art/galeria_api/GaleriaApiApplication.java
@@ -0,0 +1,13 @@
+package com.galeria.art.galeria_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaleriaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaleriaApiApplication.class, args);
+	}
+
+}
