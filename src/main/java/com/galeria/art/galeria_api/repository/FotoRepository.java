5c396d83e3d56f723a43011a7323746ae9d1ea6e
ThiagoRuan <thiagoruan46@gmail.com>
2025-09-23 17:47:20 -0300

    Repositories: FotoRepository.java criado.

---
+++ src/main/java/com/galeria/art/galeria_api/repository/FotoRepository.java
@@ -0,0 +1,17 @@
+package com.galeria.art.galeria_api.repository;
+
+import com.galeria.art.galeria_api.models.Foto;
+import com.galeria.art.galeria_api.models.Album;
+import com.galeria.art.galeria_api.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface FotoRepository extends JpaRepository<Foto, Long> {
+    List<Foto> findByOwner(User owner);
+    List<Foto> findByAlbum(Album album);
+    List<Foto> findByAutor(String autor);
+    List<Foto> findByExtensao(String extensao);
+    List<Foto> findByDataUploadYear(int ano);
+}
