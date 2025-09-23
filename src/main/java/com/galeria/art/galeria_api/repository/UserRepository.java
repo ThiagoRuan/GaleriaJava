cb1b44b07d58fe2c61a0040fd2070b48404e2082
ThiagoRuan <thiagoruan46@gmail.com>
2025-09-23 17:46:14 -0300

    Repositories: UserRepository.java atualizado.

--- src/main/java/com/galeria/art/galeria_api/repository/UserRepository.java
+++ src/main/java/com/galeria/art/galeria_api/repository/UserRepository.java
@@ -1,8 +1,9 @@
-package com.galeria.art.galeria_api.repository
+package com.galeria.art.galeria_api.repository;
 
 import com.galeria.art.galeria_api.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
