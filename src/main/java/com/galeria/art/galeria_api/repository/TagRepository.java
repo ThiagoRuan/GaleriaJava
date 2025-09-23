950a6ddc43939555d2dee443ce8e8057c181d348
ThiagoRuan <thiagoruan46@gmail.com>
2025-09-23 17:47:40 -0300

    Repositories: TagRepository.java criado.

---
+++ src/main/java/com/galeria/art/galeria_api/repository/TagRepository.java
@@ -0,0 +1,11 @@
+package com.galeria.art.galeria_api.repository;
+
+import com.galeria.art.galeria_api.models.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface TagRepository extends JpaRepository<Tag, Long> {
+    Optional<Tag> findByNome(String nome);
+}
