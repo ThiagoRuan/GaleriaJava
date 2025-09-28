905ac339d1905a68e890b54bb5ea4a149a9eb368
ThiagoRuan <thiagoruan46@gmail.com>
2025-09-28 18:34:44 -0300

    Controllers: AuthController criado.

---
+++ src/main/java/com/galeria/art/galeria_api/controllers/AuthController.java
@@ -0,0 +1,29 @@
+package com.galeria.art.galeria_api.controllers;
+
+import com.galeria.art.galeria_api.dto.UserRegisterDTO;
+import com.galeria.art.galeria_api.services.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Void> registrarUsuario(@Valid @RequestBody UserRegisterDTO userDTO) {
+        userService.registrarUsuario(userDTO);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
