3a4969d42ec7697da317f3c9e66e243f51888870
ThiagoRuan <thiagoruan46@gmail.com>
2025-09-28 21:19:20 -0300

    DTOs: LoginResponseDto criado.

---
+++ src/main/java/com/galeria/art/galeria_api/dto/LoginResponseDto.java
@@ -0,0 +1,10 @@
+package com.galeria.art.galeria_api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class LoginResponseDto {
+    private String token;
+}
