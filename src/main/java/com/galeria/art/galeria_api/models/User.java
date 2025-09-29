0f15adb3d17388626f7530ca64be2ca8f17dd091
ThiagoRuan <thiagoruan46@gmail.com>
2025-09-28 21:08:39 -0300

    Models (User): implements UserDetails.

--- src/main/java/com/galeria/art/galeria_api/models/User.java
+++ src/main/java/com/galeria/art/galeria_api/models/User.java
@@ -1,15 +1,19 @@
 package com.galeria.art.galeria_api.models;
 
 import jakarta.persistence.*;
+
+import java.util.Collection;
 import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
     // ID do usuário.
     @Id
@@ -37,5 +41,39 @@ public class User {
     // Álbuns do usuário.
     @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Album> albuns;
-    
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.senha;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
