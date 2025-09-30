f6fea24fca7e742b0ae946514966e23d40b3d2bb
ThiagoRuan <thiagoruan46@gmail.com>
2025-09-29 21:44:26 -0300

    Services: AlbumService criado.

---
+++ src/main/java/com/galeria/art/galeria_api/services/AlbumService.java
@@ -0,0 +1,53 @@
+package com.galeria.art.galeria_api.services;
+
+import com.galeria.art.galeria_api.dto.AlbumDTO;
+import com.galeria.art.galeria_api.dto.FotoDTO;
+import com.galeria.art.galeria_api.exceptions.ItemNotFoundException;
+import com.galeria.art.galeria_api.models.Album;
+import com.galeria.art.galeria_api.models.Foto;
+import com.galeria.art.galeria_api.models.User;
+import com.galeria.art.galeria_api.repositories.AlbumRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class AlbumService {
+
+    private final AlbumRepository albumRepository;
+    private final ModelMapper modelMapper;
+
+    public List<AlbumDTO> findAlbumsByOwner(User owner) {
+        List<Album> albuns = albumRepository.findByOwner(owner);
+
+        return albuns.stream()
+                .map(album -> modelMapper.map(album, AlbumDTO.class))
+                .collect(Collectors.toList());
+    }
+
+    public FotoDTO cover(Long albumId) {
+
+        Foto cover = albumRepository.findById(albumId)
+                .orElseThrow(() -> new ItemNotFoundException("Album '"+albumId+"' não encontrado."))
+                .getCoverFoto();
+
+        return modelMapper.map(cover, FotoDTO.class);
+    }
+
+    public Set<FotoDTO> fotos(Long albumId) {
+
+        Set<Foto> fotos = albumRepository.findById(albumId)
+                .orElseThrow(() -> new ItemNotFoundException("Album '"+albumId+"' não encontrado."))
+                .getFotos();
+
+        return fotos.stream()
+                .map(foto -> modelMapper.map(foto, FotoDTO.class))
+                .collect(Collectors.toSet());
+    }
+
+}
