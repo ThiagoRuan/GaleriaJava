b0425ab2f5ef6226466fa5ca8223d82f461941da
ThiagoRuan <thiagoruan46@gmail.com>
2025-09-30 23:14:06 -0300

    AlbumUpdateDTO criado.

---
+++ src/main/java/com/galeria/art/galeria_api/dto/AlbumUpdateDTO.java
@@ -0,0 +1,11 @@
+package com.galeria.art.galeria_api.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AlbumUpdateDTO {
+    private String titulo;
+    private Long coverId;
+}
