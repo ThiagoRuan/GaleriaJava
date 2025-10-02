2a7dbf647313212df46f6d7ecd8de2701e26af9c
ThiagoRuan <thiagoruan46@gmail.com>
2025-10-01 22:40:52 -0300

    Swagger adicionado.

--- src/main/java/com/galeria/art/galeria_api/securities/SecurityConfiguration.java
+++ src/main/java/com/galeria/art/galeria_api/securities/SecurityConfiguration.java
@@ -23,7 +23,12 @@ public class SecurityConfiguration {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers(
+                                "/auth/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
