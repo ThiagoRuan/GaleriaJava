e0ee001348270f29e90d37c79af614811807463d
ThiagoRuan <thiagoruan46@gmail.com>
2025-10-10 16:58:24 -0300

    @Pattern adicionado nos UpdateDTO

--- src/main/java/com/galeria/art/galeria_api/dto/AlbumUpdateDTO.java
+++ src/main/java/com/galeria/art/galeria_api/dto/AlbumUpdateDTO.java
@@ -1,11 +1,13 @@
 package com.galeria.art.galeria_api.dto;
 
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class AlbumUpdateDTO {
+    @Pattern(regexp = ".*\\S.*", message = "O título não pode estar em branco.")
     private String titulo;
     private Long coverId;
 }
--- src/main/java/com/galeria/art/galeria_api/dto/FotoUpdateDTO.java
+++ src/main/java/com/galeria/art/galeria_api/dto/FotoUpdateDTO.java
@@ -1,11 +1,14 @@
 package com.galeria.art.galeria_api.dto;
 
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class FotoUpdateDTO {
+    @Pattern(regexp = ".*\\S.*", message = "O nome não pode estar em branco.")
     private String nome;
+    @Pattern(regexp = ".*\\S.*", message = "O autor não pode estar em branco.")
     private String autor;
 }
